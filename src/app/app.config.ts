05af5526e05ab14fc1aa0c0ac5a9f4d2c864c7a3
m-aouzal <aouzal1999@gmail.com>
2024-06-14 00:14:56 +0100

    refactor: Update sendTransaction method in DashboardComponent

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 const firebaseConfig = {
   apiKey: 'AIzaSyDLsVB0EtrGzpNCEUga9RnnzM93olJpek0',
   authDomain: 'hederarewarddistribution.firebaseapp.com',
@@ -24,6 +25,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()),
+    provideAuth(() => getAuth()), provideFirebaseApp(() => initializeApp({"projectId":"hederarewarddistribution","appId":"1:1007886719824:web:5730274d0883e2b4a9aa55","databaseURL":"https://hederarewarddistribution-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"hederarewarddistribution.appspot.com","apiKey":"AIzaSyDLsVB0EtrGzpNCEUga9RnnzM93olJpek0","authDomain":"hederarewarddistribution.firebaseapp.com","messagingSenderId":"1007886719824","measurementId":"G-0YXDC93X6C"})), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideFirebaseApp(() => initializeApp({"projectId":"hederarewarddistribution","appId":"1:1007886719824:web:5730274d0883e2b4a9aa55","databaseURL":"https://hederarewarddistribution-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"hederarewarddistribution.appspot.com","apiKey":"AIzaSyDLsVB0EtrGzpNCEUga9RnnzM93olJpek0","authDomain":"hederarewarddistribution.firebaseapp.com","messagingSenderId":"1007886719824","measurementId":"G-0YXDC93X6C"})), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()),
   ],
 };
